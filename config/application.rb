f273781747bda3b0845d0bb7125217d8fdc613d5
RaulFit <fitraul6@gmail.com>
2024-10-01 18:32:18 +0300

    Implemented option to switch the language between english and romanian

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,7 @@ module LearningPlatform
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+    I18n.available_locales = %i[en ro]
     config.i18n.default_locale = :en
   end
 end
